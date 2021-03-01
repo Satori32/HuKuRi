d05718382851948cbe0dcfa84de2c0fddc77c898
Satori32 <Huga8989+githubf@gmail.com>
2021-03-01 10:17:37 +0900

    fix logic

--- ソース.cpp
+++ ソース.cpp
@@ -3,7 +3,7 @@
 
 std::uint64_t HuKuRi(double Yen, double Risoku, std::uint64_t Hi) {
 	for (std::uint64_t i = 0; i < Hi; i++) {
-		Yen = Yen + (Yen * (Risoku / Hi));
+		Yen = Yen + (Yen * ((Risoku/100.0) / Hi));
 	}
 
 	return Yen;
@@ -11,13 +11,14 @@ std::uint64_t HuKuRi(double Yen, double Risoku, std::uint64_t Hi) {
 
 int main() {
 	std::uint64_t Yen = 2000000;
-	double Risoku = 0.3;
+	double Risoku = 10;
 	std::uint64_t Hi = 365;
 	std::uint64_t Nen = 3;
+	std::uint64_t N = Yen;
 
 	for (std::uint64_t i = 0; i < Nen; i++) {
-		Yen = HuKuRi(Yen, Risoku, Hi);
-		std::cout << Yen << std::endl;
+		N = HuKuRi(N, Risoku, Hi);
+		std::cout << N<<' '<<N/(double)Yen << std::endl;
 	}
 	return 0;
 }
